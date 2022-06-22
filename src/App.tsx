055e77357cedb569a28858b28326245143453469
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-22 16:06:07 -0300

    add Event page component at App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query MyQuery {
-  lessons {
-    id
-    slug
-    title
-    teacher {
-      name
-      bio
-      avatarURL
-    }
-  }
-}
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
 
   return (
-    <ul>
-      {data?.lessons.map(lesson => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
+    <Event />
   )
 }
 
