360e24ae3a2fe9198e8d74a0cd591cfff719b2e3
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-21 23:28:23 -0300

    create first client

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query MyQuery {
+  lessons {
+    id
+    slug
+    title
+    teacher {
+      name
+      bio
+      avatarURL
+    }
+  }
+}
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+
   return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello World!</h1>
+    <ul>
+      {data?.lessons.map(lesson => (
+        <li key={lesson.id}>{lesson.title}</li>
+      ))}
+    </ul>
   )
 }
 
