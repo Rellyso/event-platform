a79c1c412600d4c3e94a761cbd6b7bf6e79eca0c
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-25 09:48:09 -0300

    add authorization from CMS

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4o7301v0x1201xx9n52dr60/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
